43075d34be7dc5965f7a242f76f705b3acb489d9
Raj Aryan <154800358+Rajaryan827@users.noreply.github.com>
2024-12-01 15:03:34 +0000

    added the bmw logo and done so many changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello World</h1>
+      <img className="im" src="./bmw.png" alt="image of heart"></img>
       <h2>Start editing to see !</h2>
     </div>
   );
